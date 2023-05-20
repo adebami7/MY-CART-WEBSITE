354fa18a1f1a4eba7be569503559bd9ac7c172df
adebami7 <fiyinfoluwademide@gmail.com>
2023-05-20 10:57:05 +0100

    new change

--- src/products.js
+++ src/products.js
@@ -1,8 +1,8 @@
 import product1 from "./assets/products/1.png";
 import product2 from "./assets/products/2.png";
 import product3 from "./assets/products/3.png";
-import product4 from "./assets/products/4.png";
-import product5 from "./assets/products/5.png";
+import product4 from "./assets/products/4-new.png";
+import product5 from "./assets/products/5-new.png";
 import product6 from "./assets/products/6.webp";
 import product7 from "./assets/products/7.webp";
 import product8 from "./assets/products/8.webp";
@@ -11,37 +11,37 @@ export const PRODUCTS = [
   {
     id: 1,
     productName: "IPhone",
-    price: 999.0,
+    price: 1199.0,
     productImage: product1,
   },
   {
     id: 2,
     productName: "Macbook Pro 2022 (M1)",
-    price: 1999.0,
+    price: 2299.0,
     productImage: product2,
   },
   {
     id: 3,
     productName: "Cannon M50 Camera",
-    price: 699.0,
+    price: 999.0,
     productImage: product3,
   },
   {
     id: 4,
     productName: "WLS Van Gogh Denim Jacket",
-    price: 228.0,
+    price: 299.99,
     productImage: product4,
   },
   {
     id: 5,
     productName: "LED Light Strips",
-    price: 19.99,
+    price: 899.99,
     productImage: product5,
   },
   {
     id: 6,
-    productName: "SPECTRUM LS TEE",
-    price: 68.0,
+    productName: "DON'T TRIP TEE",
+    price: 49.99,
     productImage: product6,
   },
   {
@@ -52,8 +52,8 @@ export const PRODUCTS = [
   },
   {
     id: 8,
-    productName: "DON'T TRIP UNSTRUCTURED HAT",
-    price: 40.0,
+    productName: "DON'T TRIP LS HAT",
+    price: 58.99,
     productImage: product8,
   },
 ];
